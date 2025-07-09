5e492c26cf57a0d28074214c27023c65dc27be58
Carl Lester P. Paldeng <carlpaldeng.official@gmail.com>
2025-07-09 08:47:00 +0800

    Updated Love Calculator with If-else

--- 12-advance-javascript/15.1-love-calculator.js
+++ 12-advance-javascript/15.1-love-calculator.js
@@ -12,8 +12,11 @@ rl.question("What is your name? ", function(name1) {
     console.log("Hello, " + name1 + " and " + name2 +"!");
   var lovescore = Math.random() * 100;
   lovescore = Math.floor(lovescore) + 1;
+  if (lovescore >= 70){
+  console.log("Your love score is " + lovescore + "%" + " like I love you!");
+  }else{
   console.log("Your love score is " + lovescore + "%");
+  }
   rl.close();
-
     });
 });
