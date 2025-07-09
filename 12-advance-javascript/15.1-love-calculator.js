ad965061f300c7e85c679fed1f1cb03fbbffe3cc
Carl Lester P. Paldeng <carlpaldeng.official@gmail.com>
2025-07-09 08:52:54 +0800

    Js with Operators and Multiple If Statements

--- 12-advance-javascript/15.1-love-calculator.js
+++ 12-advance-javascript/15.1-love-calculator.js
@@ -12,10 +12,14 @@ rl.question("What is your name? ", function(name1) {
     console.log("Hello, " + name1 + " and " + name2 +"!");
   var lovescore = Math.random() * 100;
   lovescore = Math.floor(lovescore) + 1;
-  if (lovescore >= 70){
+  if (lovescore > 80){
   console.log("Your love score is " + lovescore + "%" + " like I love you!");
-  }else{
-  console.log("Your love score is " + lovescore + "%");
+  }
+  if (lovescore >= 30 && lovescore <= 70){
+  console.log("Your love score is " + lovescore + "%" + " like it's too low");  
+  }
+  if (lovescore <= 30){
+  console.log("Your love score is " + lovescore + "%" + " like it's too low");  
   }
   rl.close();
     });
