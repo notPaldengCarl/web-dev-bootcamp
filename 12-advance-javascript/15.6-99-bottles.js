386edc72e55918c8d413ba8571eb0df8c4d810d2
Carl Lester P. Paldeng <carlpaldeng.official@gmail.com>
2025-07-09 11:29:01 +0800

    Updated Bottles

--- 12-advance-javascript/15.6-99-bottles.js
+++ 12-advance-javascript/15.6-99-bottles.js
@@ -1,12 +1,31 @@
-var bottle = 99
+let bottle = 99;
 
-while (bottle >= 0) {
-    var bottleWord = "bottle";
-    if (bottle === 1) {
-        bottleWord = "bottles";
-    }
-    console.log(bottle + " " + bottleWord + " of the beer on the wall"+","+ " " + bottle + " " + bottleWord + " of the beer");
-    console.log("Take one down, pass it around")
-    bottle--,
-     console.log(bottle + " " + bottleWord + " of the beer");
-    }
+while (bottle > 0) {
+  let bottleWord = bottle === 1 ? "bottle" : "bottles";
+
+  console.log(
+    bottle + " " + bottleWord + " of beer on the wall, " +
+    bottle + " " + bottleWord + " of beer."
+  );
+
+  bottle--;
+
+  let nextBottleWord = bottle === 1 ? "bottle" : "bottles";
+
+  if (bottle > 0) {
+    console.log(
+      "Take one down, pass it around, " +
+      bottle + " " + nextBottleWord + " of beer on the wall.\n"
+    );
+  } else {
+    console.log(
+      "Take one down, pass it around, no more bottles of beer on the wall.\n"
+    );
+  }
+}
+
+// Final verse after loop ends
+console.log(
+  "No more bottles of beer on the wall, no more bottles of beer.\n" +
+  "Go to the store and buy some more, 99 bottles of beer on the wall."
+);
